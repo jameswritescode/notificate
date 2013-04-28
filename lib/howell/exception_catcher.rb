e7695b63708356bc362d59f3977386fcdee7b60d
James Newton <james@Zaphyous.com>
2013-04-27 22:32:06 -0400

    Howell::ExceptionCatcher should use Howell::Notice.new rather then trying to call an instance method

--- lib/howell/exception_catcher.rb
+++ lib/howell/exception_catcher.rb
@@ -2,6 +2,6 @@ module Howell::ExceptionCatcher
   def self.notify(exception, data)
     payload = { body: { payload: { exception: exception, data: data } } }
 
-    ::Howell::Notice.exception_notice(payload)
+    ::Howell::Notice.new(payload, :exception)
   end
 end
