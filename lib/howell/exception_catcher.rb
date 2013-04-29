6d584f787bd6b059703f62bd21bf5077c92f87f7
James Newton <james@Zaphyous.com>
2013-04-29 15:59:00 -0400

    call #to_s on the exception to hopefully prevent the crap failures array

--- lib/howell/exception_catcher.rb
+++ lib/howell/exception_catcher.rb
@@ -1,6 +1,6 @@
 module Howell::ExceptionCatcher
   def self.notify(exception, data)
-    payload = { body: { payload: { exception: exception, data: data } }.to_json }
+    payload = { body: { payload: { exception: exception.to_s, data: data } }.to_json }
 
     ::Howell::Notice.new(payload, :exception)
   end
